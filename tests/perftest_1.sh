94cd7770a2e6f5f045788b8d232f2182da4e9615
Uli Hitzel <uli.hitzel@gmail.com>
2018-09-14 19:24:21 +0800

    added perfest

---
+++ tests/perftest_1.sh
@@ -0,0 +1,1 @@
+wrk -d 60 -c 5 -t 5 -H "Authorization: Bearer eyJpIjogMCwgInUiOiAiYWRtaW4iLCAidCI6ICIxNTM2OTIyOTMwIn0=.edf9047bf583b125ba68be137fccdb4c7db0577e8348a7160a88686eae063959" http://localhost:8080/users/1
