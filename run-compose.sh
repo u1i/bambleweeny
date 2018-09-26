6304af1b520393c7f43f60a8f304637eef8cfd2c
Uli Hitzel <uli.hitzel@gmail.com>
2018-09-26 21:55:07 +0800

    docker compose will now always create fresh instances

--- run-compose.sh
+++ run-compose.sh
@@ -2,5 +2,6 @@ echo Getting docker-compose YAML...
 curl -sSL -o /tmp/bwy.yml https://raw.githubusercontent.com/u1i/bambleweeny/master/docker-compose.yml
 
 echo Running Docker Compose...
+docker-compose rm -f
 docker-compose pull
 docker-compose -f /tmp/bwy.yml up -d
