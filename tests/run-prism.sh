27cdad5e81896c9bd1b2dc4fc366c88f9837556c
Uli <uli.hitzel@gmail.com>
2018-11-05 10:38:10 +0800

    added prism mocking server

---
+++ tests/run-prism.sh
@@ -0,0 +1,4 @@
+# https://help.stoplight.io/prism/getting-started
+# curl https://raw.githubusercontent.com/stoplightio/prism/master/install.sh | sh
+
+prism run --mock --list --spec https://raw.githubusercontent.com/u1i/bambleweeny/master/swagger.json
